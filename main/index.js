3244f5c8f6bb4e907f669e29b33d17cfedb10d17
Leonardo Pernomian <leonardopernomian@hotmail.com>
2024-07-13 23:17:26 -0300

    finalizando o modulo de funcoes

--- main/index.js
+++ main/index.js
@@ -33,4 +33,37 @@ function juntarNumerosEstrings(arr1, arr2) {
     return arr1.concat(arr2);
 }
 console.log(juntarNumerosEstrings([1, 2, 3], [4, 5, 6]));
-console.log(juntarNumerosEstrings([1, 2, 3], ["kjganga", "jgnaga"]));
+console.log(juntarNumerosEstrings([1, 2, 3], ["kjganga", "jgnaga"])); // faz com que se una dois tipos em arrays
+// parametros default
+function somaPadrao(n, m = 10) {
+    return n + m;
+}
+console.log(somaPadrao(10, 10));
+console.log(somaPadrao(10, 2));
+// tipo unknown
+function fazAlgo(x) {
+    if (Array.isArray(x)) {
+        console.log(x.length);
+    }
+    else if (typeof x === "number") {
+        console.log("X é um número");
+    }
+}
+fazAlgo([376383, 2, 3]);
+fazAlgo(7528652);
+// tipo never
+function mensagemErro(msg) {
+    throw new Error(msg);
+}
+// mensagemErro("ocorreu um erro!")
+// usando o rest
+function somandoVariosValores(...n) {
+    return n.reduce((number, soma) => soma + number);
+}
+console.log(somandoVariosValores(1, 2, 3));
+// destruction
+function detalhesProdutos({ name, preço }) {
+    return `O produto ${name} custa ${preço}`;
+}
+const camisa = { name: "Camisa", preço: 13.99 };
+console.log(detalhesProdutos(camisa));
--- main/index.ts
+++ main/index.ts
@@ -56,4 +56,57 @@ function juntarNumerosEstrings<T>(arr1: T[], arr2: T[]) {
 }
 
 console.log(juntarNumerosEstrings([1,2,3], [4,5,6]))
-console.log(juntarNumerosEstrings<number | string>([1,2,3], ["kjganga", "jgnaga"]))
+console.log(juntarNumerosEstrings<number | string>([1,2,3], ["kjganga", "jgnaga"])) // faz com que se una dois tipos em arrays
+
+
+// parametros default
+
+function somaPadrao(n: number, m = 10) {
+    return n + m
+}
+
+console.log(somaPadrao(10, 10))
+console.log(somaPadrao(10, 2))
+
+
+// tipo unknown
+
+function fazAlgo(x: unknown) {
+    if(Array.isArray(x)) {
+        console.log(x.length)
+    } else if(typeof x === "number") {
+        console.log("X é um número")
+    }
+}
+
+fazAlgo([376383,2,3])
+fazAlgo(7528652)
+
+
+// tipo never
+
+function mensagemErro(msg: string): never {
+    throw new Error(msg)
+}
+
+// mensagemErro("ocorreu um erro!")
+
+// usando o rest
+
+function somandoVariosValores(...n: number[]){
+    return n.reduce((number, soma) => soma + number)
+}
+
+
+console.log(somandoVariosValores(1,2,3))
+
+
+// destruction
+
+function detalhesProdutos({name, preço}: {name: string, preço: number}):string{
+    return `O produto ${name} custa ${preço}`
+}
+
+const camisa = {name: "Camisa", preço: 13.99}
+
+console.log(detalhesProdutos(camisa))
