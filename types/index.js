7c2f6a21885db965ad3d07335b10ae267119e708
Leonardo Pernomian <leonardopernomian@hotmail.com>
2024-07-17 19:45:51 -0300

    avançando em tipos

--- types/index.js
+++ types/index.js
@@ -10,3 +10,36 @@ const carro1 = { nome: "Pegout", rodas: 4, motor: 1.0, cor: "verde" };
 const caneta1 = { nome: "minha caneta", rodas: false, motor: false, cor: "azul" };
 console.log(carro1);
 console.log(caneta1);
+// type params
+function pegarChave(obj, key) {
+    // return `A chave ${key} está presente no objeto e tem valor de ${obj[key]}`
+}
+const servidor = {
+    hr: '2TB',
+    ram: '32GB'
+};
+console.log(pegarChave(servidor, 'ram'));
+function mostrarNome(obj, nome) {
+    return `${obj[nome]}`;
+}
+const leozik = {
+    nome: 'Leo',
+    idade: 19,
+    temCNH: true
+};
+console.log(mostrarNome(leozik, 'nome'));
+// typeof types
+const qualquerNome = 'leo';
+const qualquerNome2 = 'mateus';
+const qualquerNome3 = 'lucas';
+const novoCaminhao = {
+    km: 1000,
+    kg: 1000,
+    descrisao: 'teste'
+};
+function exibirKM(km) {
+    console.log(`o caminhao tem ${km} Km`);
+}
+exibirKM(novoCaminhao.km);
+const minhaVar = 16;
+const testando = "text";
--- types/index.ts
+++ types/index.ts
@@ -27,3 +27,88 @@ const caneta1: caneta = {nome: "minha caneta", rodas: false, motor: false, cor: 
 
 console.log(carro1);
 console.log(caneta1)
+
+
+// type params
+
+function pegarChave<T, K extends keyof T>(obj: T, key: K) {
+    // return `A chave ${key} está presente no objeto e tem valor de ${obj[key]}`
+}
+
+const servidor = {
+    hr: '2TB',
+    ram: '32GB'
+}
+
+console.log(pegarChave(servidor, 'ram'))
+
+// keyof type operator
+
+type Pessoa = {nome: string, idade: number, temCNH: boolean}
+
+type P = keyof Pessoa // representa todas as chaves do obj
+
+function mostrarNome(obj: Pessoa, nome: P): string { // o nome puxa o tipo do tipo pessoa
+    return `${obj[nome]}`
+}
+
+const leozik: Pessoa = {
+    nome: 'Leo',
+    idade: 19,
+    temCNH: true
+}
+
+console.log(mostrarNome(leozik, 'nome'))
+
+// typeof types
+
+const qualquerNome: string = 'leo'
+
+const qualquerNome2: typeof qualquerNome = 'mateus'
+
+const qualquerNome3: typeof qualquerNome2 = 'lucas'
+
+
+// indexed acess type
+
+type Caminhao = {km: number, kg: number, descrisao: string}
+
+
+type km = Caminhao['km'] // representa apenas uma acha ao obj
+
+const novoCaminhao: Caminhao = {
+    km: 1000,
+    kg: 1000,
+    descrisao: 'teste'
+}
+
+function exibirKM(km: km) {
+    console.log(`o caminhao tem ${km} Km`)
+}
+
+exibirKM(novoCaminhao.km)
+
+
+// condicional types
+
+interface A {}
+
+interface B extends A {}
+
+type meuTipo = B extends A ? number : string
+
+const minhaVar: meuTipo = 16
+// const minhaVar2: meuTipo = 'bafa'
+
+
+// template literal types
+
+type teste1 = "text"
+
+type outroTipo = teste1
+
+
+const testando: outroTipo = "text"
+
+
+
